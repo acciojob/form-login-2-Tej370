6187d9ec5593465aa2b5dc4e2a0772c49b837c32
Tej370 <153205267+Tej370@users.noreply.github.com>
2025-01-11 12:33:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function display() {
+    let fName = document.getElementById("First Name").value;
+    let lName = document.getElementById("Last Name").value;
+    let number = document.getElementById("Phone Number").value;
+    let email = document.getElementById("Email ID").value;
+ 
+    let message = "First Name: " + fName + " " +
+                  "Last Name: " + lName + " " +
+                  "Phone Number: " + number + " " +
+                  "Email ID: " + email;
+ 
+    alert(message);
+}
